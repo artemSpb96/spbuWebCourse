311674dac13dff142e2b1c6bead77db4542e5e48
artemSpb96 <artem.ananchenko@yandex.ru>
2017-12-02 20:45:42 +0300

    Create example jetty project

---
+++ src/main/java/webapp/Main.java
@@ -0,0 +1,15 @@
+package webapp;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler handler = new ServletContextHandler(server, "/example");
+        handler.addServlet(ExampleServlet.class, "/");
+        server.start();
+    }
+
+}
